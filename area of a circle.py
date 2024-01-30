a740a70c061dfcb67cdf80204df064a1598df1f1
Syeda_Ayesha <156789992+ItsAyes@users.noreply.github.com>
2024-01-30 15:45:23 +0500

    Add files via upload

---
+++ area of a circle.py
@@ -0,0 +1,4 @@
+r=float(input("Enter the radius of circle: "))
+pie=float(3.142)
+A = 2*pie*r 
+print("The area of a circle is: ",A)
---
+++ my average final.py
@@ -0,0 +1,5 @@
+num1=int(input("Enter your first number here: "))
+num2=int(input("Enter your second number here: "))
+sum= num1+num2
+average= sum/2
+print("The average of two numbers is", average)
